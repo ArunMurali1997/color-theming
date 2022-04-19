f4906226eb9b24184df4349500f1ec5c9641de00
Arun Murali <arun.murali@secondopinionexpert.com>
2022-04-19 22:44:41 +0530

    converted to react using webpack

---
+++ public/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\Users\\\\arunm\\\\Desktop\\\\Office Projects\\\\colorTheming\\\\index.js: Support for the experimental syntax 'jsx' isn't currently enabled (8:3):\\n\\n\\u001b[0m \\u001b[90m  6 |\\u001b[39m \\u001b[36mconst\\u001b[39m root \\u001b[33m=\\u001b[39m \\u001b[33mReactDOMClient\\u001b[39m\\u001b[33m.\\u001b[39mcreateRoot(rootElement)\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m  7 |\\u001b[39m root\\u001b[33m.\\u001b[39mrender(\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m  8 |\\u001b[39m   \\u001b[33m<\\u001b[39m\\u001b[33mReact\\u001b[39m\\u001b[33m.\\u001b[39m\\u001b[33mStrictMode\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m   \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m  9 |\\u001b[39m     \\u001b[33m<\\u001b[39m\\u001b[33mApp\\u001b[39m \\u001b[33m/\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 10 |\\u001b[39m   \\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mReact\\u001b[39m\\u001b[33m.\\u001b[39m\\u001b[33mStrictMode\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 11 |\\u001b[39m )\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\nAdd @babel/preset-react (https://git.io/JfeDR) to the 'presets' section of your Babel config to enable transformation.\\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://git.io/vb4yA) to the 'plugins' section to enable parsing.\\n    at instantiate (C:\\\\Users\\\\arunm\\\\Desktop\\\\Office Projects\\\\colorTheming\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:72:32)\\n    at constructor (C:\\\\Users\\\\arunm\\\\Desktop\\\\Office Projects\\\\colorTheming\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:358:12)\\n    at Parser.raise (C:\\\\Users\\\\arunm\\\\Desktop\\\\Office Projects\\\\colorTheming\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:3335:19)\\n    at Parser.expectOnePlugin (C:\\\\Users\\\\arunm\\\\Desktop\\\\Office Projects\\\\colorTheming\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:3392:18)\\n    at Parser.parseExprAtom (C:\\\\Users\\\\arunm\\\\Desktop\\\\Office Projects\\\\colorTheming\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12970:18)\\n    at Parser.parseExprSubscripts (C:\\\\Users\\\\arunm\\\\Desktop\\\\Office Projects\\\\colorTheming\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12540:23)\\n    at Parser.parseUpdate (C:\\\\Users\\\\arunm\\\\Desktop\\\\Office Projects\\\\colorTheming\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12519:21)\\n    at Parser.parseMaybeUnary (C:\\\\Users\\\\arunm\\\\Desktop\\\\Office Projects\\\\colorTheming\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12490:23)\\n    at Parser.parseMaybeUnaryOrPrivate (C:\\\\Users\\\\arunm\\\\Desktop\\\\Office Projects\\\\colorTheming\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12284:61)\\n    at Parser.parseExprOps (C:\\\\Users\\\\arunm\\\\Desktop\\\\Office Projects\\\\colorTheming\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12291:23)\");\n\n//# sourceURL=webpack://colortheming/./index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./index.js"]();
+/******/ 	
+/******/ })()
+;
